578b00f7875821388820f2e3583cca88eb00a50b
etay <etay21@gmail.com>
2017-03-26 20:30:49 +0300

    eeee

--- 3-tier/BL/Product_Managment_BL.cs
+++ 3-tier/BL/Product_Managment_BL.cs
@@ -31,7 +31,7 @@ namespace BL
             itsDAL.AddProduct(p);
         }
     }
-    console.etay
+    console.etay21
         public List<Product> FindProductByName(string name)
         {
            public List<Product> FindProductByName(string name)
